3d25667e61361af60b1c2c3920a5b8f2ecb794ac
Tejas <tejaskothari03@gmail.com>
2023-09-10 17:50:26 +0530

    Added backend starter code & dependencies

---
+++ src/backend/src/test/java/com/growthvvork/colorflow/ColorflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growthvvork.colorflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
