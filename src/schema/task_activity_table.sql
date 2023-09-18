932f0f51f43e0ab0a3a1eadc040a158e1a72b136
Tejas <tejaskothari03@gmail.com>
2023-09-19 01:17:32 +0530

    Cleaned up schema files

--- src/schema/task_activity_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS colorflow.task_activity(
-    task_activity_id CHAR(36),
-	create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	task_id INT,
-    PRIMARY KEY (task_activity_id),
-    FOREIGN KEY (task_id) REFERENCES colorflow.task(task_id)
-    ON DELETE NO ACTION ON UPDATE NO ACTION
-);
